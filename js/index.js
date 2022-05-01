f0109fb2c1f603a6a35878337704a3bcc018b071
Martin Huna <martinhuna85@gmail.com>
2022-05-01 21:54:38 +0100

    Counter added into Index.js + linked counter back to index.html

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
-// document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 0
+// 33:20 youtube vid
+
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
 
 function increment() {
-    console.log("The button was clicked")
+    count = count + 1
+    countEl.innerText = count
 }
+
