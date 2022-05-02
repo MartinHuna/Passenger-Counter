9546c3e21f3cb72ebcba87808f9b84cc455c5d31
Martin Huna <martinhuna85@gmail.com>
2022-05-02 17:40:28 +0100

    Last update to index.js - fixed save + count = 0

--- js/index.js
+++ js/index.js
@@ -3,14 +3,17 @@ let count = 0
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 let saveEl = document.getElementById("save-el")
 
 function save() {
     let totalVisits = count + " - "
-    saveEl.innerText += totalVisits
-
-    console.log(count)
+    saveEl.textContent += totalVisits
+    countEl.textContent = 0
+    count = 0
 }
-// youtube video: paused at 1:05:39 Good night!!!
+
+// First App is built :)
+
+
