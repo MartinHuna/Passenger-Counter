25b9285af04f6072e3a3aaaeff36f057d74f7840
Martin Huna <martinhuna85@gmail.com>
2022-05-02 01:39:37 +0100

    Amended index.js file

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-let countEl = document.getElementById("count-el")
-let count = 0
+//let countEl = document.getElementById("count-el")
+//let count = 0
 
-function increment() {
-    count = count + 1
-    countEl.innerText = count
-}
+//function increment() {
+//    count = count + 1
+//    countEl.innerText = count
+//}
 
-function save() {
-    console.log(count)
-}
+//function save() {
+//    console.log(count)
+//}
 
+let name = "Martin"
+let greeting = "Hi, my name is "
+let myGreeting = greeting + name
 
+console.log(myGreeting)
